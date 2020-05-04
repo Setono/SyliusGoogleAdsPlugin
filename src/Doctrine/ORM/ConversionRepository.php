60f41be77f1f1c1fb9dce6a80979eccdb8611a3f
Joachim Løvgaard <joachim@loevgaard.dk>
2020-05-04 09:56:19 +0200

    Refactored method to return what we need when we want to insert tags

--- src/Doctrine/ORM/ConversionRepository.php
+++ src/Doctrine/ORM/ConversionRepository.php
@@ -10,12 +10,14 @@ use Sylius\Component\Channel\Model\ChannelInterface;
 
 class ConversionRepository extends EntityRepository implements ConversionRepositoryInterface
 {
-    public function findEnabledByChannel(ChannelInterface $channel): array
+    public function findEnabledByChannelAndCategory(ChannelInterface $channel, string $category): array
     {
         return $this->createQueryBuilder('o')
             ->andWhere(':channel MEMBER OF o.channels')
             ->andWhere('o.enabled = true')
+            ->andWhere('o.category = :category')
             ->setParameter('channel', $channel)
+            ->setParameter('category', $category)
             ->getQuery()
             ->getResult()
         ;
