fccccffc61db9fe16c595a03b4251111b2dc9934
Joachim Løvgaard <joachim@loevgaard.dk>
2021-12-06 13:53:28 +0100

    Upgrade to Sylius 1.10

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -1,10 +1,12 @@
 <?php
 
-use Tests\Setono\SyliusGoogleAdsPlugin\Application\Kernel;
-use Symfony\Component\Debug\Debug;
+declare(strict_types=1);
+
+use Tests\Acme\SyliusExamplePlugin\Application\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
