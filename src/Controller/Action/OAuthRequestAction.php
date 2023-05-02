9a7f30bbc820acafa20b9d77c9d7cddce5371d3a
Joachim Løvgaard <joachim@loevgaard.dk>
2023-05-02 10:24:28 +0200

    Make the oauth flow better and more robust

---
+++ src/Controller/Action/OAuthRequestAction.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusGoogleAdsPlugin\Controller\Action;
+
+use Setono\SyliusGoogleAdsPlugin\Model\ConnectionInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * This OAuth request is based on the example from the Google Ads PHP library:
+ * https://github.com/googleads/google-ads-php/blob/v19.0.0/examples/Authentication/GenerateUserCredentials.php
+ */
+final class OAuthRequestAction extends OAuthAction
+{
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        private readonly RepositoryInterface $connectionRepository,
+    ) {
+        parent::__construct($urlGenerator);
+    }
+
+    public function __invoke(Request $request, int $connectionId): Response
+    {
+        /** @var ConnectionInterface|null $connection */
+        $connection = $this->connectionRepository->find($connectionId);
+        if (null === $connection) {
+            throw new NotFoundHttpException(sprintf('A connection with id %d does not exist', $connectionId));
+        }
+
+        try {
+            $state = bin2hex(random_bytes(16));
+            $request->getSession()->set(self::SESSION_STATE_NAME, $state);
+            $request->getSession()->set(self::SESSION_CONNECTION_ID, $connectionId);
+
+            $oauth2 = $this->buildOAuth($connection, $state);
+
+            return new RedirectResponse((string) $oauth2->buildFullAuthorizationUri([
+                'access_type' => 'offline',
+            ]));
+        } catch (\Throwable $e) {
+            return $this->addFlashAndRedirect(
+                $request,
+                $e->getMessage(),
+                $this->urlGenerator->generate('setono_sylius_google_ads_admin_connection_update', ['id' => $connectionId]),
+            );
+        }
+    }
+}
---
+++ src/Controller/Action/OAuthResponseAction.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusGoogleAdsPlugin\Controller\Action;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Setono\DoctrineObjectManagerTrait\ORM\ORMManagerTrait;
+use Setono\SyliusGoogleAdsPlugin\Model\ConnectionInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Webmozart\Assert\Assert;
+
+final class OAuthResponseAction extends OAuthAction
+{
+    use ORMManagerTrait;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        private readonly RepositoryInterface $connectionRepository,
+        ManagerRegistry $managerRegistry,
+    ) {
+        parent::__construct($urlGenerator);
+
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $session = $request->getSession();
+
+        try {
+            /** @var mixed $connectionId */
+            $connectionId = $session->get(self::SESSION_CONNECTION_ID);
+            Assert::integer($connectionId);
+
+            $expectedState = $session->get(self::SESSION_STATE_NAME);
+            Assert::stringNotEmpty($expectedState);
+
+            $actualState = $request->query->get('state');
+            Assert::same($expectedState, $actualState);
+
+            $code = $request->query->get('code');
+            Assert::stringNotEmpty($code, 'The OAuth response did not contain a code');
+        } catch (\InvalidArgumentException $e) {
+            return $this->addFlashAndRedirect(
+                $request,
+                $e->getMessage(),
+                $this->urlGenerator->generate('setono_sylius_google_ads_admin_connection_index'),
+            );
+        }
+
+        /** @var ConnectionInterface|null $connection */
+        $connection = $this->connectionRepository->find($connectionId);
+        if (null === $connection) {
+            throw new NotFoundHttpException(sprintf('A connection with id %d does not exist', $connectionId));
+        }
+
+        $oauth2 = $this->buildOAuth($connection, $expectedState);
+        $oauth2->setCode($code);
+        $credentials = $oauth2->fetchAuthToken();
+        if (!isset($credentials['access_token']) || !is_string($credentials['access_token'])) {
+            throw new \InvalidArgumentException('The access token is not valid');
+        }
+
+        $connection->setAccessToken($credentials['access_token']);
+
+        $this->getManager($connection)->flush();
+
+        return $this->addFlashAndRedirect(
+            $request,
+            'setono_sylius_google_ads.access_token_updated',
+            $this->urlGenerator->generate('setono_sylius_google_ads_admin_connection_update', ['id' => $connection->getId()]),
+            'success',
+        );
+    }
+}
