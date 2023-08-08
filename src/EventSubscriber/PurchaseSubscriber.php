b7567e07737b94c942d3cc8e0e99506a56a010f4
Joachim Løvgaard <joachim@loevgaard.dk>
2023-08-08 14:54:47 +0200

    Make the purchase subscriber more robust

--- src/EventSubscriber/PurchaseSubscriber.php
+++ src/EventSubscriber/PurchaseSubscriber.php
@@ -6,6 +6,9 @@ namespace Setono\SyliusGoogleAdsPlugin\EventSubscriber;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Setono\DoctrineObjectManagerTrait\ORM\ORMManagerTrait;
 use Setono\SyliusGoogleAdsPlugin\Event\PrePersistConversionFromOrderEvent;
 use Setono\SyliusGoogleAdsPlugin\Factory\ConversionFactoryInterface;
@@ -15,9 +18,11 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Webmozart\Assert\Assert;
 
-final class PurchaseSubscriber implements EventSubscriberInterface
+final class PurchaseSubscriber implements EventSubscriberInterface, LoggerAwareInterface
 {
     use ORMManagerTrait;
+
+    private LoggerInterface $logger;
 
     public function __construct(
         ManagerRegistry $managerRegistry,
@@ -27,6 +32,7 @@ final class PurchaseSubscriber implements EventSubscriberInterface
         private readonly string $cookieName,
     ) {
         $this->managerRegistry = $managerRegistry;
+        $this->logger = new NullLogger();
     }
 
     public static function getSubscribedEvents(): array
@@ -38,30 +44,38 @@ final class PurchaseSubscriber implements EventSubscriberInterface
 
     public function track(ResourceControllerEvent $event): void
     {
-        /** @var mixed|OrderInterface $order */
-        $order = $event->getSubject();
-        Assert::isInstanceOf($order, OrderInterface::class);
+        try {
+            /** @var mixed|OrderInterface $order */
+            $order = $event->getSubject();
+            Assert::isInstanceOf($order, OrderInterface::class);
 
-        $request = $this->requestStack->getMainRequest();
-        if (null === $request) {
-            return;
+            $request = $this->requestStack->getMainRequest();
+            Assert::notNull($request);
+
+            $gclid = $request->cookies->get($this->cookieName);
+            if (!is_string($gclid) || '' === $gclid) {
+                return;
+            }
+
+            $conversion = $this->conversionFactory->createFromOrder($order);
+            $conversion->setGoogleClickId($gclid);
+
+            $userAgent = $request->headers->get('User-Agent');
+            Assert::stringNotEmpty($userAgent);
+
+            $this->eventDispatcher->dispatch(new PrePersistConversionFromOrderEvent($conversion, $order));
+
+            $this->getManager($conversion)->persist($conversion);
+        } catch (\Throwable $e) {
+            $this->logger->error(sprintf(
+                'An error occurred when trying to track a Google Ads conversion: %s',
+                $e->getMessage(),
+            ));
         }
+    }
 
-        $gclid = $request->cookies->get($this->cookieName);
-        if (!is_string($gclid) || '' === $gclid) {
-            return;
-        }
-
-        $conversion = $this->conversionFactory->createFromOrder($order);
-        $conversion->setGoogleClickId($gclid);
-
-        $userAgent = $request->headers->get('User-Agent');
-        if (is_string($userAgent) && '' !== $userAgent) {
-            $conversion->setUserAgent($userAgent);
-        }
-
-        $this->eventDispatcher->dispatch(new PrePersistConversionFromOrderEvent($conversion, $order));
-
-        $this->getManager($conversion)->persist($conversion);
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
