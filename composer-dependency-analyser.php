000b44edb5d9e011b021e198dcec3f0b9d78178e
Joachim Løvgaard <joachim@loevgaard.dk>
2024-09-17 08:47:07 +0200

    Add new dependency analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,9 @@
+<?php
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('setono/client-bundle', ErrorType::DEV_DEPENDENCY_IN_PROD)
+;
